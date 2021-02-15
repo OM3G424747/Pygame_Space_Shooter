1f0d23d4072bd208be5a25188365ed32b6b76c31
OM3G424747 <44314634+OM3G424747@users.noreply.github.com>
2021-02-16 01:13:16 +0200

    Update Basic Game.py
    Added player movement

--- Basic Game.py
+++ Basic Game.py
@@ -2,13 +2,41 @@ import pygame
 
 Screen_Width = 800
 Screen_Height = 800
-Screen_Title = "Space Flight Pygame" #Sets name on gamewindow 
+Screen_Title = "Space Lander Pygame" #Sets name on gamewindow 
 White_Colour = (255,255,255) #colours set according to RGB - R 255 G 255  B 255
 Black_Colour = (0,0,0)
 Red_Colour = (255,0,0)
 Green_Colour = (0,255,0)
 Blue_Colour = (0,0,255)
 Clock = pygame.time.Clock()
+
+
+class GameObject: #class for defining game objects that will be drawn onto the game screen and moved arround
+    def __init__(self, image_path, X_pos, Y_pos, Width, Height):
+        Object_Image = pygame.image.load(image_path) #Loads image to be set
+        self.Image = pygame.transform.scale(Object_Image, (Width, Height)) #Scales the image that's been loaded in 
+        self.X_pos = X_pos
+        self.Y_pos = Y_pos
+        self.Width = Width
+        self.Height = Height
+    
+    def Draw (self,background):
+        background.blit(self.Image,(self.X_pos,self.Y_pos)) #Blit funtion is used to draw imaghes to the game screen/ surface selected along with the X and Y Pos taken in the form of a tuple 
+
+class PlayerCharacter(GameObject):
+    
+    Speed = 10 
+
+    def __init__ (self, image_path, X_pos, Y_pos, Width, Height):
+       GameObject.__init__(self, image_path, X_pos, Y_pos, Width, Height)
+    
+    def Move(self, direction):
+        if direction > 0:
+            self.Y_pos -= self.Speed 
+        elif direction < 0:
+            self.Y_pos += self.Speed
+
+
 
 class Game:
     Tick_Rate = 60 #Change to set framerate
@@ -24,25 +52,32 @@ class Game:
     
     def run_game_loop (self):
         Is_Game_Over = False
+        direction = 0
+        Player1 = PlayerCharacter("ship.png", 375,700,75,75) #creates player 1 ship character 
+
         while Is_Game_Over == False: #game loop checks if the game is over and will repeat until the condition is met and the game over state is set to True
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     Is_Game_Over = True
+                elif event.type == pygame.KEYDOWN: #Checks when the up or down key is pressed by the user 
+                    if event.key == pygame.K_UP:
+                        direction = 1
+                    elif event.key == pygame.K_DOWN:
+                        direction = -1
+                elif event.type == pygame.KEYUP: #Checks when the key is released 
+                    if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                        direction = 0
+
                 print(event)
+            self.Game_Screen.fill(Black_Colour)
+            Player1.Move(direction)
+            Player1.Draw (self.Game_Screen)
+         
+
             pygame.display.update() #Updates the current frame after completing the loop
             Clock.tick(self.Tick_Rate) #Sets the frame rate per second
     
-class GameObject: #class for defining game objects that will be drawn onto the game screen and moved arround
-    def __init__(self, image_path, X_pos, Y_pos, Width, Height):
-        Object_Image = pygame.image.load(image_path) #Loads image to be set
-        self.Image = pygame.transform.scale(Object_Image, (Width, Height)) #Scales the image that's been loaded in 
-        self.X_pos = X_pos
-        self.Y_pos = Y_pos
-    
-    def draw (self,background):
-        background.blit(self.Image,(self.X_pos,self.Y_pos)) #Blit funtion is used to draw imaghes to the game screen/ surface selected along with the X and Y Pos taken in the form of a tuple 
-       
-Player1 = GameObject("ship.png", 375,300,75,75) #creates player 1 ship character a
+
 
 
 
@@ -52,9 +87,9 @@ pygame.init()
 new_game = Game(Screen_Title,Screen_Width,Screen_Height) #creates a new game part of the "game class"
 
 
-Player1.draw (new_game.Game_Screen)
 
 new_game.run_game_loop() #Starts the game loop as defined in the class to continue looping until the game over state becomes true 
+
 
 
 pygame.quit()
